6b69552a36c9a5760b4592a8ef5bd4bb98e7c615
SHarika05 <116783656+SHarika05@users.noreply.github.com>
2022-10-28 10:02:30 +0530

    Add files via upload

---
+++ CUT/src/main.c
@@ -0,0 +1,12 @@
+#include<functions.h>
+#include<stdlib.h>
+#include<stdio.h>
+int main( )
+{
+	
+	printf("\n\t\t**************Welcome to Library Management System************ ");
+
+	options( );
+	return EXIT_SUCCESS;
+}
+
